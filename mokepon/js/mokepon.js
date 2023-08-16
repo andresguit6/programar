1c1f68115053a3beb07e467d1c080ceb3a174102
Andres Gutierrez <andresguit6@gmail.com>
2023-08-15 20:34:40 -0500

    mokepon

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,13 +1,22 @@
+let ataqueJugador 
+
 function iniciarJuego () {
     let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener("click", selecionarMascotaJugador)
+
+    let botonFuego = document.getElementById ("boton-fuego")
+    botonFuego.addEventListener("clic" , ataquefuego)
+    let botonAgua = document.getElementById ("boton-agua")
+    botonAgua.addEventListener("clic" , ataqueAgua)
+    let botonTierra = document.getElementById ("boton-tierra") 
+    botonTierra.addEventListener("clic" , ataqueTierra)
+
 }
 
 function selecionarMascotaJugador () {
     let inputHipodoge = document.getElementById ("Hipodoge")
     let inputcapipepo = document.getElementById ("capipepo")
     let inputratigueya = document.getElementById ("ratigueya")
-    let inputbotonmascota = document.getElementById ("boton-mascota")
     let spanMascotaJugador = document.getElementById ("mascota-jugador")
 
     if (inputHipodoge.checked) {
@@ -19,5 +28,39 @@ function selecionarMascotaJugador () {
     } else {
         alert("seleciona una mascota")
     }
+
+    selecionarMascotaEnemigo ()
+
 }
+
+function selecionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio (1,3)
+    let spanMascotaEnemigo = document.getElementById ("mascota-enemigo")
+     
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = "hipodogue "
+    } else if (ataqueAleatorio == 2 ) {
+        spanMascotaEnemigo.innerHTML = "capipepo "
+    } else {
+        spanMascotaEnemigo.innerHTML = "ratigueya "
+    }
+
+}
+
+function ataquefuego () {
+    ataqueJugador = "fuego"
+}   alert (ataqueJugador)
+
+function ataqueAgua () {
+    ataqueJugador = "agua"
+}   alert (ataqueJugador)
+
+function ataqueTierra () {
+    ataqueJugador = "tierra"
+}   alert (ataqueJugador)
+
+function aleatorio (min,max){
+        return Math.floor(Math.random()* ( max - min + 1 ) + min)
+} 
+
 window.addEventListener("load", iniciarJuego)
